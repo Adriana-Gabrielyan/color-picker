b922e39279abc2062fc825831f3b3327eb0644d4
Adriana <adrin.gabrielyan97@mail.ru>
2021-08-01 22:39:08 +0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'color-picker';
+  colors: any = [];
+  size = 100;
+  styleObject(): Object {
+  
+    return { width: this.size / 10};
+  }
+  generateColorBoxes() {
+    for (let i = 1; i <= this.size; i++) {
+      this.colors.push({
+        id: i,
+        color: 'red',
+      });
+    }
+  }
 }
